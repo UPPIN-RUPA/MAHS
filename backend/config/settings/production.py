7ea208be6ef7bef504ad4c6b7b98a283be889864
UPPIN-RUPA <66957046+UPPIN-RUPA@users.noreply.github.com>
2026-03-26 17:06:40 -0400

    Prepare MAHS for deployment

--- backend/config/settings/production.py
+++ backend/config/settings/production.py
@@ -4,14 +4,25 @@ from .base import *  # noqa: F403,F401
 
 
 DEBUG = False
+ALLOWED_HOSTS = ["*"]
 
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.postgresql",
-        "NAME": os.getenv("DB_NAME"),
-        "USER": os.getenv("DB_USER"),
-        "PASSWORD": os.getenv("DB_PASSWORD"),
-        "HOST": os.getenv("DB_HOST", "localhost"),
-        "PORT": os.getenv("DB_PORT", "5432"),
+USE_POSTGRES = os.getenv("DB_NAME") is not None
+
+if USE_POSTGRES:
+    DATABASES = {
+        "default": {
+            "ENGINE": "django.db.backends.postgresql",
+            "NAME": os.getenv("DB_NAME"),
+            "USER": os.getenv("DB_USER"),
+            "PASSWORD": os.getenv("DB_PASSWORD"),
+            "HOST": os.getenv("DB_HOST", "localhost"),
+            "PORT": os.getenv("DB_PORT", "5432"),
+        }
     }
-}
+else:
+    DATABASES = {
+        "default": {
+            "ENGINE": "django.db.backends.sqlite3",
+            "NAME": BASE_DIR / "db.sqlite3",  # noqa: F405
+        }
+    }
